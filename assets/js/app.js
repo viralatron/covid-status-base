460a288797496510c877f3c4c2e03c1340747113
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2021-10-29 20:42:39 -0300

    finished frontpage charts

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,17 +20,85 @@ async function getData(url) {
 
 async function GetDashboard() {
   const data = await getSummary().then((d) => {
-    const data = d.data;
+    let data = d.data;
     // totals
-    const confirmed = document.getElementById(`confirmed`);
-    confirmed.innerText = data.Global.TotalConfirmed;
+    SetTotals(
+      data.Global.TotalConfirmed,
+      data.Global.TotalDeaths,
+      data.Global.TotalRecovered,
+      data.Global.Date
+    );
 
-    const death = document.getElementById(`death`);
-    death.innerText = data.Global.TotalDeaths;
-
-    const recovered = document.getElementById(`recovered`);
-    recovered.innerText = data.Global.TotalRecovered;
     // newCases
+    SetNewCases(
+      data.Global.NewConfirmed,
+      data.Global.NewRecovered,
+      data.Global.NewDeaths
+    );
+
     // total per countries
+    SetCountriesTotal(data.Countries);
+  });
+}
+
+function SetTotals(TotalConfirmed, TotalDeaths, TotalRecovered, QueryDate) {
+  const confirmed = document.getElementById(`confirmed`);
+  confirmed.innerText = TotalConfirmed;
+
+  const death = document.getElementById(`death`);
+  death.innerText = TotalDeaths;
+
+  const recovered = document.getElementById(`recovered`);
+  recovered.innerText = TotalRecovered;
+
+  const lastUpdate = document.getElementById(`date`);
+  lastUpdate.innerText += ` ${QueryDate}`;
+}
+
+function SetNewCases(NewConfirmed, NewRecovered, NewDeaths) {
+  const ctx = document.getElementById(`pizza`).getContext(`2d`);
+
+  const myChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: ["confirmados", "recuperados", "mortes"],
+      datasets: [
+        {
+          label: "distribuição de novos casos",
+          data: [NewConfirmed, NewRecovered, NewDeaths],
+          backgroundColor: [`#FF6384`, `#36A2EB`, `#FFCD56`],
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
+}
+
+function SetCountriesTotal(Countries) {
+  let TopTen = _.slice(_.orderBy(Countries, ["TotalDeaths"], ["desc"]), 0, 10);
+  let TTLabels = _.map(TopTen, "Country");
+  let TTData = _.map(TopTen, "TotalDeaths");
+
+  const truncAppend = (n) =>
+    `#${_.truncate(n.ID, { length: 6, omission: "" })}`;
+
+  let TTColors = _.map(TopTen, truncAppend);
+  console.log(TTColors);
+
+  const ctx = document.getElementById(`barras`).getContext(`2d`);
+
+  const myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: TTLabels,
+      datasets: [
+        {
+          label: "Total de Mortes por país",
+          data: TTData,
+          backgroundColor: TTColors,
+          borderWidth: 1,
+        },
+      ],
+    },
   });
 }
