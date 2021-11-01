4180053ee447fa28eaa85764285cc8b13d734187
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2021-11-01 09:10:42 -0300

    finished mostly SetCounrtyData, SetTimelineByStatus
    must find a way to update SetTimelineByStatus' canvas

--- assets/js/country.js
+++ assets/js/country.js
@@ -8,7 +8,20 @@ async function getCountries() {
 }
 function getCountryAllStatus(country, from, to) {
   try {
-    const data = axios.get(`https://api.covid19api.com/country/${country}`);
+    const fromDate = from;
+    const toDate = to;
+
+    let url = `https://api.covid19api.com/country/${country}`;
+
+    if (from || to) {
+      url += "?";
+      if (from) url += `from=${fromDate}T00:00:00Z`;
+      if (from && to) url += `&`;
+      if (to) url += `to=${toDate}T00:00:00Z`;
+    }
+
+    const data = axios.get(url);
+
     return data;
   } catch (err) {
     console.log(err);
@@ -23,9 +36,9 @@ document.getElementById("filtro").addEventListener("click", SetCountryData);
 
 async function SetCountries() {
   const select = document.getElementById("cmbCountry");
-  const countries = await getCountries();
+  let countries = await getCountries();
 
-  _.forEach(countries.data, (d) => {
+  _.forEach(_.orderBy(countries.data, "Country"), (d) => {
     const option = document.createElement("option");
     option.value = d.Slug;
     option.innerText = d.Country;
@@ -38,97 +51,54 @@ async function SetCountryData() {
   const dateEnd = document.getElementById("date_end");
   const country = document.getElementById("cmbCountry");
   const dataType = document.getElementById("cmbData");
-  console.log(
-    getCountryAllStatus(
-      country.value,
-      dateStart.value,
-      dateEnd.value,
-      dataType.value
-    )
-  );
+  getCountryAllStatus(
+    country.value,
+    dateStart.value,
+    dateEnd.value,
+    dataType.value
+  ).then((d) => SetData(d.data, dataType));
 }
 
-async function GetDashboard() {
-  const data = await getSummary().then((d) => {
-    let data = d.data;
-    // totals
-    SetTotals(
-      data.Global.TotalConfirmed,
-      data.Global.TotalDeaths,
-      data.Global.TotalRecovered,
-      data.Global.Date
-    );
+function SetTotals(totalConfirmed, totalDeaths, totalRecovered) {
+  const confirmed = document.getElementById(`kpiconfirmed`);
+  confirmed.innerText = totalConfirmed;
 
-    // newCases
-    SetNewCases(
-      data.Global.NewConfirmed,
-      data.Global.NewRecovered,
-      data.Global.NewDeaths
-    );
+  const death = document.getElementById(`kpideaths`);
+  death.innerText = totalDeaths;
 
-    // total per countries
-    SetCountriesTotal(data.Countries);
-  });
+  const recovered = document.getElementById(`kpirecovered`);
+  recovered.innerText = totalRecovered;
 }
 
-function SetTotals(TotalConfirmed, TotalDeaths, TotalRecovered, QueryDate) {
-  const confirmed = document.getElementById(`confirmed`);
-  confirmed.innerText = TotalConfirmed;
+function SetData(data, type) {
+  const totalConfirmed = _.sumBy(data, "Confirmed");
+  const totalDeaths = _.sumBy(data, "Deaths");
+  const totalRecovered = _.sumBy(data, "Recovered");
 
-  const death = document.getElementById(`death`);
-  death.innerText = TotalDeaths;
+  SetTotals(totalConfirmed, totalDeaths, totalRecovered);
 
-  const recovered = document.getElementById(`recovered`);
-  recovered.innerText = TotalRecovered;
-
-  const lastUpdate = document.getElementById(`date`);
-  lastUpdate.innerText += ` ${QueryDate}`;
+  SetTimelineByStatus(data, type);
 }
 
-function SetNewCases(NewConfirmed, NewRecovered, NewDeaths) {
-  const ctx = document.getElementById(`pizza`).getContext(`2d`);
+function SetTimelineByStatus(data, type) {
+  console.log(data);
 
-  const myChart = new Chart(ctx, {
-    type: "pie",
+  const ctx = document.getElementById(`linhas`).getContext(`2d`);
+
+  const config = {
+    type: "line",
     data: {
-      labels: ["confirmados", "recuperados", "mortes"],
-      datasets: [
-        {
-          label: "distribuição de novos casos",
-          data: [NewConfirmed, NewRecovered, NewDeaths],
-          backgroundColor: [`#FF6384`, `#36A2EB`, `#FFCD56`],
-          borderWidth: 1,
-        },
-      ],
-    },
-  });
-}
-
-function SetCountriesTotal(Countries) {
-  let TopTen = _.slice(_.orderBy(Countries, ["TotalDeaths"], ["desc"]), 0, 10);
-  let TTLabels = _.map(TopTen, "Country");
-  let TTData = _.map(TopTen, "TotalDeaths");
-
-  const truncAppend = (n) =>
-    `#${_.truncate(n.ID, { length: 6, omission: "" })}`;
-
-  let TTColors = _.map(TopTen, truncAppend);
-  console.log(TTColors);
-
-  const ctx = document.getElementById(`barras`).getContext(`2d`);
-
-  const myChart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: TTLabels,
+      labels: _.map(data, "Date"),
       datasets: [
         {
           label: "Total de Mortes por país",
-          data: TTData,
-          backgroundColor: TTColors,
+          data: _.map(data, type),
+          backgroundColor: "red",
           borderWidth: 1,
         },
       ],
     },
-  });
+  };
+
+  const myChart = new Chart(ctx, config);
 }
